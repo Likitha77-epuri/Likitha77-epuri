a468d0d30a0cace669da1f1c2e640cbd85d99e2c
likitha77-epuri <likithaeepuri77@gmail.com>
2026-01-31 00:17:09 +0530

    day 4

--- add.py
+++ add.py
@@ -1,30 +1,22 @@
-def convert_string_to_int(list_a):
-    new_list = []
-    for item in list_a:
-        num = int(item)
+def get_sum_list(int_list):
+    con_sum_list=[]
+    end_index=len(int_list)-1
+    for i in range(end_index):
+        sum_list=int_list[i]+int_list[i+1]
+        con_sum_list.append(sum_list)
+    return con_sum_list
+def print_triangle(int_list):
+    while len(int_list)>1:
+        con_sum_list=get_sum_list(int_list)
+        print(con_sum_list)
+        int_list=con_sum_list
+def convert_string_to_int(str_num_list):
+    new_list=[]
+    for i in str_num_list:
+        num=int(i)
         new_list.append(num)
     return new_list
-
-
-m, n = input().split()
-m, n = int(m), int(n)
-num_list = []
-
-for i in range(m):
-    list_a = input().split()
-    list_a = convert_string_to_int(list_a)
-    num_list.append(list_a)
-
-# Write your code here
-row_wise_max=[]
-for row in num_list:
-    row_wise_max.append(max(row))
-maxi=max(row_wise_max)
-row_index=row_wise_max.index(maxi)
-max_row=num_list[row_index]
-print(max_row)
-column_index=max_row.index(maxi)
-max_column=[]
-for row in num_list:
-    max_column.append(row[column_index])
-print(max_column)
+str_num_list=input().split(",")
+int_list=convert_string_to_int(str_num_list)
+print(int_list)
+print_triangle(int_list)
