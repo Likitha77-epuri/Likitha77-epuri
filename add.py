a9f6502d2d48aad0c3360760434d5eb3a7c33a5f
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-01 21:34:26 +0530

    day 5.1

--- add.py
+++ add.py
@@ -1,26 +1,22 @@
-def convert_to_string(str_list):
-    int_list=[]
-    for str_num in str_list:
-        num=int(str_num)
-        int_list.append(num)
-    return int_list
-def convert_to_list(keys_list,value_list):
-    dict_a={}
-    n_keys=len(keys_list)
-    for i in range(n_keys):
-        key=keys_list[i]
-        value=value_list[i]
-        dict_a[key]=value 
-    return dict_a
-dict_a_keys=input().split()
-dict_a_values=input().split()
-dict_b_keys=input().split()
-dict_b_values=input().split()
-dict_a_values=convert_to_string(dict_a_values)
-dict_b_values=convert_to_string(dict_b_values)
-student_details1=convert_to_list(dict_a_keys,dict_a_values)
-student_details2=convert_to_list(dict_b_keys,dict_b_values)
-student_details1.update(student_details2)
-student_details=student_details1.items()
-student_details=sorted(student_details)
-print(student_details)
+def get_sum_list(int_list):
+    con_sum_list=[]
+    end_index=len(int_list)-1
+    for i in range(end_index):
+        sum_list=int_list[i]+int_list[i+1]
+        con_sum_list.append(sum_list)
+    return con_sum_list
+def print_triangle(int_list):
+    while len(int_list)>1:
+        con_sum_list=get_sum_list(int_list)
+        print(con_sum_list)
+        int_list=con_sum_list
+def convert_string_to_int(str_num_list):
+    new_list=[]
+    for i in str_num_list:
+        num=int(i)
+        new_list.append(num)
+    return new_list
+str_num_list=input().split(",")
+int_list=convert_string_to_int(str_num_list)
+print(int_list)
+print_triangle(int_list)
