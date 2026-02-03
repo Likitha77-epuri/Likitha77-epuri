362ba6f2443e3c1277de5202e2d0be84e797b9e8
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-03 22:05:24 +0530

    sum&avg

--- add.py
+++ add.py
@@ -1,26 +1,14 @@
-e=input()
-op_index=-1
-for i in range(1,len(e)):
-    if e[i] in "+-*/%":
-        op_index=i 
-        break
-if op_index==-1:
-    print("Invalid expression")
+s=input()
+total=0
+count=0
+for ch in s:
+    if ch.isdigit():
+        total+=int(ch)
+        count+=1 
+if count>0:
+    avg=round(total/count,2)
+    print(total)
+    print(avg)
 else:
-        op=e[op_index]
-        a=int(e[:op_index])
-        b=int(e[op_index+1:])
-        if op=="+":
-            print(a+b)
-        elif op=="-":
-                print(a-b)
-        elif op=="*":
-            print(a*b)
-        elif op=="/":
-            if b!=0:
-                print(a/b)
-            else:
-                print("Division by zero not allowed")
-        elif op=="%":
-            print(a%b)
-        
+    print(0)
+    print(0)
