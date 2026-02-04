d08006f627cda0d50ba38be121ce2a3d017fa0a7
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-05 00:41:13 +0530

    inheritance

--- add.py
+++ add.py
@@ -1,4 +1,5 @@
-# Implement the Car class appropriately
+# Implement the Car and Truck class appropriately
+# Inherit the Car class into Truck class and override the methods which have extra features
 class Car:
     def __init__(self, color, max_speed, acceleration, tyre_friction):
         self.color=color
@@ -6,7 +7,8 @@ class Car:
         self.acceleration=acceleration
         self.tyre_friction=tyre_friction
         self.is_engine_started=False
-        self.current_speed=0
+        self.current_speed=0 
+        
 
     def start_engine(self):
         self.is_engine_started=True
@@ -19,7 +21,10 @@ class Car:
             print("Car has not started yet")
         else:
             self.current_speed+=self.acceleration
-            
+            if self.current_speed>self.max_speed:
+                self.current_speed=self.max_speed
+                
+
     def apply_brakes(self):
         self.current_speed-=self.tyre_friction
         if self.current_speed<0:
@@ -32,10 +37,54 @@ class Car:
             print("Car has not started yet")
 
 
+class Truck(Car):
+    def __init__(self, color, max_speed, acceleration, tyre_friction, max_cargo_weight):
+        super().__init__(color,max_speed,acceleration,tyre_friction)
+        self.max_cargo_weight=max_cargo_weight
+        self.load=0 
+
+    def load_cargo(self,cargo_weight):
+        if self.is_engine_started:
+            print("Cannot load cargo during motion")
+        elif cargo_weight+self.load>self.max_cargo_weight:
+            print("Cannot load cargo more than max limit: {}".format(self.max_cargo_weight))
+        else:
+            self.load+=cargo_weight
+
+    def unload_cargo(self,cargo_weight):
+        if self.is_engine_started:
+            print("Cannot unload cargo during motion")
+        else:
+            self.load-=cargo_weight
+            if self.load<0:
+                self.load=0 
+
+    def sound_horn(self):
+        if self.is_engine_started:
+            print("Honk Honk")
+        else:
+            print("Car has not started yet")
+
+
 # You need not change any code below.
 # Do not call this function anywhere. It will automatically be called internally during tests.
 def default_test():
- car = Car(color="Red", max_speed=250, acceleration=10, tyre_friction=3)
- car.sound_horn() # Calling sound_horn() method when the is_engine_started is False
- car.start_engine() # Starting the engine
- car.sound_horn() # Calling the accelerate method when the is_engine_started is True
+    truck = Truck(color="Red", max_speed=250, acceleration=10, tyre_friction=3, max_cargo_weight=100)
+    print(truck.is_engine_started)
+    truck.load_cargo(cargo_weight=50)  # Loading cargo_weight 50 to the truck
+    print(truck.load)  # 0 + 50 => 50
+    truck.unload_cargo(cargo_weight=25)  # Unloading cargo_weight 25 from the truck
+    print(truck.load)  # 50 - 25 => 25
+    truck.unload_cargo(cargo_weight=70)  # Unloading cargo_weight 70 (more than load in the truck)
+    print(truck.load)  # 25 - 75 => 0 as load never be negative
+    truck.load_cargo(cargo_weight=120)  # Prints "Cannot load cargo more than max limit: 100"
+
+    truck.load_cargo(cargo_weight=20)  # Loading cargo_weight 20 to the truck
+    truck.start_engine()
+    print(truck.is_engine_started)  # True
+    truck.load_cargo(cargo_weight=40)  # Prints "Cannot load cargo during motion"
+    truck.unload_cargo(cargo_weight=10)  # Prints "Cannot unload cargo during motion"
+
+    truck.sound_horn()  # Prints "Honk Honk"
+    truck.stop_engine()
+    truck.sound_horn()  # Prints "Car has not started yet"
