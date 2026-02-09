5629f9b0966d62469bafaef3e337944f43b06279
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-09 21:47:41 +0530

    triangle function

--- add.py
+++ add.py
@@ -1,13 +1,8 @@
-def replace_old_value_with_new_value(matrix, old_value, new_value):
+def print_lower_triangle(matrix):
     # Complete this function
-    update=[]
-    for row in matrix:
-        update_row=row
-        for i in range(len(row)):
-            if row[i]==old_value:
-                update_row[i]=new_value
-        update.append(update_row)
-    return update
+    for i in range(len(matrix)):
+        row=matrix[i][:i+1]
+        print(row)
 
 def convert_string_to_int(list_a):
     new_list = []
@@ -26,11 +21,5 @@ for i in range(m):
     list_a = convert_string_to_int(list_a)
     num_list.append(list_a)
 
-values = input().split()
-old_value, new_value = convert_string_to_int(values)
-
-# Call the replace_old_value_with_new_value function
-# Write your code here
-update=replace_old_value_with_new_value(num_list,old_value,new_value)
-for row in update:
-    print(row)
+# Call the print_lower_triangle function
+print_lower_triangle(num_list)
