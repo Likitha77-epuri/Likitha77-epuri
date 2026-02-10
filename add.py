3278c418958233f2ac6de707d0fb6894d7a9c71b
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-10 22:35:31 +0530

    Exception

--- add.py
+++ add.py
@@ -1,15 +1,10 @@
-def string_to_int(list_a):
-    new_list=[]
-    for i in list_a:
-        num=int(i)
-        new_list.append(num)
-    return new_list
-num_list=input().split()
-num_list=string_to_int(num_list)
-num_set=set(num_list)
-if len(num_set)==1:
-    print("True")
-else:
-    num_list=list(num_set)
-    num_list.sort()
-    print(num_list)
+try:
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    c = a/b
+    print(c)
+except ZeroDivisionError:
+    print("Denominator can't be 0")
+except ValueError:
+    print("Input should be an integer")
