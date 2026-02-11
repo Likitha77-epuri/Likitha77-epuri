3f890770dd720458387f3b416a1bab0ee421a7ac
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-11 22:27:28 +0530

    item

--- add.py
+++ add.py
@@ -1,10 +1,10 @@
-try:
-    a, b = input().split()
-    a = int(a)
-    b = int(b)
-    c = a/b
-    print(c)
-except ZeroDivisionError:
-    print("Denominator can't be 0")
-except ValueError:
-    print("Input should be an integer")
+class Item:
+    def __init__(self, name, price, category):
+        if price <= 0:
+            raise ValueError("Invalid value for price, got {}".format(price))
+        self.name = name
+        self.price = price
+        self.category = category
+
+    def get_detail(self):
+        return "{}@{}-{}".format(self.name, self.price, self.category)
