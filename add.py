c4cb19609f0be26cff3185f0ad1463f3f715b056
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-15 22:23:33 +0530

    print dates

--- add.py
+++ add.py
@@ -1,17 +1,9 @@
-# Implement the Car class appropriately
-class Car:
- def __init__(self,color,max_speed,acceleration,tyre_friction):
-     self.color=color
-     self.max_speed=max_speed
-     self.acceleration=acceleration
-     self.tyre_friction=tyre_friction
-
-
-# You need not change any code below.
-# Do not call this function anywhere. It will automatically be called internally during tests.
-def default_test():
- car = Car(color="Red", max_speed=250, acceleration=10, tyre_friction=3)
- print(car.color)
- print(car.max_speed)
- print(car.acceleration)
- print(car.tyre_friction)
+from datetime import datetime,timedelta
+date=input()
+date_format="%d %b %Y"
+today=datetime.strptime(date,date_format)
+yes=today-timedelta(days=1)
+day=today+timedelta(days=1)
+print(yes)
+print(today)
+print(day)
