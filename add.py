147486ec922ad234c7b6f3fa4287a70b0268d7d8
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-17 21:43:20 +0530

    weekends

--- add.py
+++ add.py
@@ -1,12 +1,17 @@
-string=input()
-l_string=string.lower()
-alpha={'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r',
-        'j':'q','k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i',
-        's':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a'}
-s=""
-for i in l_string:
-    if i in alpha.keys():
-        s+=alpha[i]
-    else:
-        s+=" "
-print(s)
+from datetime import datetime,timedelta
+d1=input()
+d2=input()
+d1_obj=datetime.strptime(d1,"%d %b %Y")
+d2_obj=datetime.strptime(d2,"%d %b %Y")
+one_day=timedelta(days=1)
+sat=0
+sun=0
+while d1_obj<=d2_obj:
+    if d1_obj.strftime("%A")=="Saturday":
+        sat+=1 
+    elif d1_obj.strftime("%A")=="Sunday":
+        sun+=1 
+    d1_obj+=one_day
+print("Saturday:" , sat)
+print("Sunday:" , sun)
+    
