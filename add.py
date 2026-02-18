30759e01a26138b0b54906eb67bbd96cd7e45be0
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-18 21:03:13 +0530

    missing number

--- add.py
+++ add.py
@@ -1,17 +1,7 @@
-from datetime import datetime,timedelta
-d1=input()
-d2=input()
-d1_obj=datetime.strptime(d1,"%d %b %Y")
-d2_obj=datetime.strptime(d2,"%d %b %Y")
-one_day=timedelta(days=1)
-sat=0
-sun=0
-while d1_obj<=d2_obj:
-    if d1_obj.strftime("%A")=="Saturday":
-        sat+=1 
-    elif d1_obj.strftime("%A")=="Sunday":
-        sun+=1 
-    d1_obj+=one_day
-print("Saturday:" , sat)
-print("Sunday:" , sun)
-    
+list_a=list(map(int,input().split()))
+set_a=set(list_a)
+max_num=max(set_a)
+for i in range(1,max_num+2):
+    if i not in set_a:
+        print(i)
+        break
