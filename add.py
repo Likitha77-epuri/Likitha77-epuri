451f456363a51c884a6c8218a9f30f6240bd5e31
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-19 21:27:25 +0530

    new year

--- add.py
+++ add.py
@@ -1,7 +1,9 @@
-list_a=list(map(int,input().split()))
-set_a=set(list_a)
-max_num=max(set_a)
-for i in range(1,max_num+2):
-    if i not in set_a:
-        print(i)
-        break
+from datetime import datetime
+date=input()
+date_format="%b %d %Y %I:%M %p"
+dt_obj=datetime.strptime(date,date_format)
+new=datetime(dt_obj.year+1,1,1)
+dt_diff=new-dt_obj
+h=dt_diff.days*24+dt_diff.seconds//3600
+m=(dt_diff.seconds//60)%60
+print("{} hours {} minutes".format(h,m))
