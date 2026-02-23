c30c7e25abc6ace435a0dfe19a87bba83119a590
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-23 20:05:30 +0530

    list of dates

--- add.py
+++ add.py
@@ -1,7 +1,9 @@
-import datetime
-zero=datetime.datetime(1970,1,1)
-u=int(input())
-seconds=datetime.timedelta(seconds=u)
-result=zero+seconds
-dt_format="%Y-%m-%d %H:%M:%S"
-print(result.strftime(dt_format))
+from datetime import datetime,timedelta
+start=input()
+end=input()
+start_dt=datetime.strptime(start,"%b %d %Y")
+end_dt=datetime.strptime(end,"%b %d %Y")
+n=(end_dt-start_dt).days
+for i in range(n+1):
+    current=start_dt+timedelta(days=i)
+    print(current)
