eef2fc1896206e7c6660dfaa0586183c3bbb2b0f
likitha77-epuri <likithaeepuri77@gmail.com>
2026-02-28 22:35:02 +0530

    attributes

--- add.py
+++ add.py
@@ -1,7 +1,13 @@
-x = "Global Variable"
-print(x) # Global Variable
+class Mobile:
+  def __init__(self, model, price):
+      self.model = model
+      self.price = price
 
-def foo():
-   print(x) # Global Variable
+mobile1 = Mobile("iPhone 17 Pro", 120000)
+mobile2 = Mobile("Samsung Galaxy S25", 80000)
 
-foo()
+print(mobile1.model)  # iPhone 17 Pro
+print(mobile1.price)  # 120000
+
+print(mobile2.model)  # Samsung Galaxy S25
+print(mobile2.price)  # 80000
