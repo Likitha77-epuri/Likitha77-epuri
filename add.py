d4a001d9a05d083ffbc73dc40250a95c6460f1a4
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-01 16:43:52 +0530

    update attributes

--- add.py
+++ add.py
@@ -1,13 +1,9 @@
-class Mobile:
-  def __init__(self, model, price):
-      self.model = model
-      self.price = price
-
-mobile1 = Mobile("iPhone 17 Pro", 120000)
-mobile2 = Mobile("Samsung Galaxy S25", 80000)
-
-print(mobile1.model)  # iPhone 17 Pro
-print(mobile1.price)  # 120000
-
-print(mobile2.model)  # Samsung Galaxy S25
-print(mobile2.price)  # 80000
+class Cart:
+   flat_discount = 0
+   min_bill = 100
+   def print_min_bill(self):
+       print(Cart.min_bill) # 200
+a = Cart()
+b = Cart()
+Cart.min_bill = 200
+b.print_min_bill() 
