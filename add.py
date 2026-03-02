e007ffeffb3f3a6a9c7bc654ace3d04e6c243597
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-02 21:44:17 +0530

    instance method

--- add.py
+++ add.py
@@ -1,9 +1,11 @@
 class Cart:
-   flat_discount = 0
-   min_bill = 100
-   def print_min_bill(self):
-       print(Cart.min_bill) # 200
+   def __init__(self):
+       self.items = {}
+   def add_item(self, item_name,quantity):
+       self.items[item_name] = quantity
+   def display_items(self):
+       print(self.items) # {'book': 3}
+
 a = Cart()
-b = Cart()
-Cart.min_bill = 200
-b.print_min_bill() 
+a.add_item("book", 3)
+a.display_items()
