b9f517367a5f0dee19c1ba05b51d871144e428d9
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-05 21:20:54 +0530

    ids

--- add.py
+++ add.py
@@ -1,6 +1,14 @@
-dict_a = {
-  'name': 'Teja',
-  'age': 15
-}
-del dict_a['age']
-print(dict_a) # {'name': 'Teja'}
+def greet_1():
+    a = "Hello"
+    print(a)
+    print(id(a))
+
+def greet_2():
+    a = "Hey"
+    print(a)
+    print(id(a))
+
+print("Namespace - 1")
+greet_1()
+print("Namespace - 2")
+greet_2()
