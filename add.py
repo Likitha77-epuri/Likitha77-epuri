442b00d47be0cd8425fa33069e10458f8fe4141f
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-06 22:21:14 +0530

    instance

--- add.py
+++ add.py
@@ -1,14 +1,11 @@
-def greet_1():
-    a = "Hello"
-    print(a)
-    print(id(a))
+class Cart:
+   def __init__(self):
+       self.items = {}
+   def add_item(self, item_name,quantity):
+       self.items[item_name] = quantity
+   def display_items(self):
+       print(self.items) # {'book': 3}
 
-def greet_2():
-    a = "Hey"
-    print(a)
-    print(id(a))
-
-print("Namespace - 1")
-greet_1()
-print("Namespace - 2")
-greet_2()
+a = Cart()
+a.add_item("book", 3)
+a.display_items()
