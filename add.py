add8a77da332bb8a3d795bdaadc2fbdb94997e4e
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-08 22:17:27 +0530

    tuple slicing

--- add.py
+++ add.py
@@ -1,6 +1,4 @@
-def greet(arg_1, arg_2):
-    print(arg_1 + " " + arg_2) # Good Morning Ram
-
-greeting = input() # Good Morning
-name = input() # Ram
-greet(greeting, name)
+tuple= ('a','b','c','d','e','f','g','h','i','j')
+print(tuple[0:2]) # ('a', 'b')
+print(tuple[-1:-3:-2]) # ('j',)
+print(tuple[1:7:2]) # ('b', 'd', 'f')
