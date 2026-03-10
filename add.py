3ae32ff930c6abd301c206219642692a5a2df606
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-10 22:55:22 +0530

    composition

--- add.py
+++ add.py
@@ -1,15 +1,28 @@
 class Product:
     def __init__(self, name):
        self.name = name
+       self.deal_price = deal_price
+
     def display_product_details(self):
-       print("Product: {}".format(self.name)) # Product: TV
+        print("Product: {}".format(self.name)) # Product: Milk
+           
+    def get_deal_price(self):
+        return self.deal_price
 
-class ElectronicItem(Product):
-    def set_warranty(self, warranty_in_months):
-        self.warranty_in_months = warranty_in_months
+class GroceryItem(Product):
+    pass
    
-    def display_electronic_product_details(self):
-        self.display_product_details()
+class Order:
+    def __init__(self):
+         self.items_in_cart = []
+     
+    def add_item(self, product, quantity):
+        self.items_in_cart.append((product, quantity))
 
-e = ElectronicItem("TV")
-e.display_electronic_product_details()
+    def display_order_details(self):
+        for product, quantity in self.items_in_cart:
+            product.display_product_details()
+            
+milk = GroceryItem("Milk")
+order.add_item(milk, 2)
+order.display_order_details()
