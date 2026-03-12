8e92656f0e634e22d54e1bdb162aca58ff9021b5
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-12 22:11:31 +0530

    class method

--- add.py
+++ add.py
@@ -1,7 +1,8 @@
-try:  
-    result = 5/0
-    print(result)  
-except ZeroDivisionError:  
-    print("Denominator can't be 0")  
-except:  
-    print("Unhandled Exception")
+class Cart:
+   flat_discount = 0
+   @classmethod
+   def update_flat_discount(cls, new_flat_discount):
+       cls.flat_discount = new_flat_discount
+
+Cart.update_flat_discount(25)
+print(Cart.flat_discount) # 25
